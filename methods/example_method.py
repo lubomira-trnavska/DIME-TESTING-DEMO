6931b9b603f445941c66343570859d5d9c8192bd
lubomira-trnavska <lubomira-trnavska>
2025-06-13 09:01:59 +0000

    i have created a sample test

--- methods/example_method.py
+++ methods/example_method.py
@@ -2,5 +2,19 @@
 
 ### Example task: Implement a method `add_two_numbers` which will take two numbers and add them together
 
+# MY METHOD
 def add_two_numbers(number1: int, number2: int):
     return number1 + number2
+
+
+## TESTS
+def test_add_two_numbers():
+    result = add_two_numbers(10, 1)
+    
+    assert result == 10
+    
+    print('pass')
+
+
+# CALL THE TEST METHODS
+test_add_two_numbers()    

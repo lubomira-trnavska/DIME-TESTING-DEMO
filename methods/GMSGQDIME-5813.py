fed5422549f8c1bb0d2f41e04e7c86b0ab494341
olegapanasevich <olegapanasevich>
2025-06-25 19:33:35 +0000

    Small changes were added to def max_values and additional unut tests were created

--- methods/GMSGQDIME-5813.py
+++ methods/GMSGQDIME-5813.py
@@ -8,7 +8,7 @@ from datetime import date
 def max_values(df):
     result = {}
     for field in df.schema.fields:
-        if isinstance(field.dataType, (IntegerType, DecimalType, DateType)) and field.name in ['year', 'price', 'release_date']:
+        if isinstance(field.dataType, (IntegerType, DecimalType, DateType)):
             max_val = df.select(spark_max(field.name)).collect()[0][0]
 
             if isinstance(field.dataType, DateType) and max_val is not None:
@@ -18,10 +18,12 @@ def max_values(df):
                 max_val = float(max_val)
 
             result[field.name] = max_val
+    print(result)
     return result
+    
 
 #TESTS
-
+# Check if max_values has a expercted results
 def test_max_values():
     # Sample test data
     data = [
@@ -40,7 +42,6 @@ def test_max_values():
 
     df = spark.createDataFrame(data, schema=schema)
     
-
     expected = {
         "year": 2022,
         "price": 18.5,
@@ -48,8 +49,73 @@ def test_max_values():
     }
 
     result = max_values(df)
-    assert result == expected, f"❌ Test failed. Expected: {expected}, got: {result}"
-    print("✅ Unit test passed!")
+    assert result == expected, f"Test test_max_values failed. Expected: {expected}, got: {result}"
+    print("👍test_max_values passed!")
+
+
+##### Check if max_values returns None for empty dataframe
+def test_empty_df():
+    schema = StructType([
+        StructField("year", IntegerType(), True),
+        StructField("price", DecimalType(10, 2), True)
+    ])
+    df = spark.createDataFrame([], schema=schema)
+
+    expected = {
+        "year": None,
+        "price": None
+    }
+    result = max_values(df)
+    assert result == expected, f"Expected: {expected}, got: {result}"
+    print("👍 test_empty_df passed")
+          
+### Check if max_values returns empty dict for no valid types and does not raise an error
+
+def test_no_valid_types():
+    data = [("A", True), ("B", False)]
+    schema = StructType([
+        StructField("name", StringType(), True),
+        StructField("is_valid", BooleanType(), True)
+    ])
+    df = spark.createDataFrame(data, schema=schema)
+
+    expected = {}
+    result = max_values(df)
+    assert result == expected, f"Expected: {expected}, got: {result}"
+    print("👍 test_no_valid_types passed")
+
+#### Check how max_values is handling duplicates(full rows/the same value in colomn)
+def duplicates_max_values():
+    # Sample test data
+    data = [
+        (2020, "Shampoo", Decimal("15.99"), True, date(2020, 5, 20)),
+        (2020, "Shampoo", Decimal("15.99"), True, date(2020, 5, 20)),
+        (2021, "Conditioner", Decimal("18.50"), True, date(2021, 8, 15)),
+        (2022, "Hair Gel", Decimal("12.00"), False, date(2022, 2, 10))
+    ]
+
+    schema = StructType([
+        StructField("year", IntegerType(), True),
+        StructField("product_name", StringType(), True),
+        StructField("price", DecimalType(10, 2), True),
+        StructField("in_production", BooleanType(), True),
+        StructField("release_date", DateType(), True)
+    ])
+
+    df = spark.createDataFrame(data, schema=schema)
+    
+    expected = {
+        "year": 2022,
+        "price": 18.5,
+        "release_date": "10/02/2022"
+    }
+
+    result = max_values(df)
+    assert result == expected, f"Test duplicates_max_values failed. Expected: {expected}, got: {result}"
+    print("👍test_max_values passed!")
 
 #CALL THE TEST
 test_max_values()
+test_empty_df()
+test_no_valid_types()
+duplicates_max_values()
